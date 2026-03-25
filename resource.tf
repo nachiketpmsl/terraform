ad2cc0de9549b7721859259a8fa610dfb1d058e7
Nachiket <nachiket.pmsl@gmail.com>
2026-03-25 21:09:27 +0000

    tets

--- resource.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "my_ec2" {
-	ami = "ami-0b6c6ebed2801a5cb"
-	instance_type = var.instance_type
-	subnet_id = "subnet-052be6ed50f932a9e"
-}
--- variable.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {
-	type = string
-}
-
-variable "instance_type" {
-	type = string
-}
